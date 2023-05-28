d6248a9552966b205ef80595d9c7966adc2c5885
AkbarBeiramzadeh <akbar.beiramzadeh@gmail.com>
2023-05-28 12:52:07 +0330

    method init added to User class

--- user.py
+++ user.py
@@ -1,2 +1,11 @@
 class User:
-    pass
+    users_dict = {}
+
+    def __init__(self,name,password,birth_date,register_date,wallet,subscription,phone_number=None):
+        self.name = name
+        self.password = password
+        self.birth_date = birth_date
+        self.register_date = register_date
+        self.wallet = wallet
+        self.subscription = subscription
+        self.phone_number = phone_number
